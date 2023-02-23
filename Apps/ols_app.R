9ff2e588a2b7ca55d0a44c491b28de1c23ba6b4a
Matthew E. Vanaman <40613941+mvanaman@users.noreply.github.com>
2023-02-23 13:01:24 -0500

    increased n, added marginals, adjusted labels

--- Apps/ols_app.R
+++ Apps/ols_app.R
@@ -1,12 +1,15 @@
-packages <- c("tidyverse", "shiny", "wesanderson", "DT")
+packages <- c("tidyverse", "shiny", "wesanderson", "DT", "ggExtra", "ggrepel")
 lapply(packages, require, character.only = TRUE)
 
 plot_guess <- function(
-  slope, y_intercept, resids_guess = FALSE, resids_OLS = FALSE, labels = FALSE
-  ){
+    slope, y_intercept, resids_guess = FALSE, resids_OLS = FALSE, labels = FALSE
+){
   # get data ----
-  x <- 1:10
-  y <- c(2, 1, 5, 3, 6, 4, 8, 10, 7, 9)
+  set.seed(350)
+  Sigma <- matrix(c(10,3,3,2),2,2)
+  data <- MASS::mvrnorm(n = 20, mu = rep(5, 2), Sigma, empirical = TRUE) %>% abs() %>% round(2)
+  x <- data[, 1]
+  y <- data[, 2]
   # fit OLS
   OLS <- lm(y ~ x)
   # statistics from guess
@@ -32,14 +35,14 @@ plot_guess <- function(
   data_tab <- data %>% 
     mutate(
       across(.cols = 3:ncol(.), round, 2), across(.cols = 3:ncol(.), format, nsmall = 2)
-      )
+    )
   # get summary Stats ----
   SSs <- tibble("Sum of Sqaured Residuals" = c(sum(data$`Squared Residual (Guess)`), sum(data$`Squared Residual (OLS)`)))
   regression_guess <- tibble("<i>Y</i>-Intercept" = y_intercept, Slope = slope, )
   regression_OLS <- tibble(
     "<i>Y</i>-Intercept" = OLS$coefficients["(Intercept)"],
     Slope = OLS$coefficients["x"]
-    )
+  )
   ## r-squared for OLS
   model_OLS <- sum((data$Y - OLS$fitted.values)^2)
   residual_OLS <- sum((data$Y - mean(data$Y))^2)
@@ -62,7 +65,7 @@ plot_guess <- function(
   reg_table_OLS <- reg_table_long %>% filter(Line == "OLS") %>% select(-Line)
   reg_table_long <- full_join(reg_table_OLS, reg_table_guess, by = "Statistic")
   reg_table_long <- rename(reg_table_long, "OLS" = Value.x, "Your Guess" = Value.y)
-    
+  
   # display plot ----
   plot <- ggplot(data, aes(x = X, y = Y)) +
     geom_point(alpha = 0.5, shape = 1) +
@@ -71,23 +74,23 @@ plot_guess <- function(
       aes(slope = Slope, intercept = `<i>Y</i>-Intercept`, color = Line), 
       size = c(1, 1.5),
       show.legend = TRUE
-      ) +
+    ) +
     # connects dots to line of best guess
     {if (resids_guess)
-        geom_segment(
-          aes(
-            x = X,
-            y = Y,
-            xend = X,
-            yend = `Predicted Y (Guess)`
-          ),
-          linetype = "dashed",
-          color = "#C27D38",
-          alpha = 0.75
-        ) 
+      geom_segment(
+        aes(
+          x = X,
+          y = Y,
+          xend = X,
+          yend = `Predicted Y (Guess)`
+        ),
+        linetype = "dashed",
+        color = "#C27D38",
+        alpha = 0.75
+      ) 
     } +
     {if (resids_guess)
-      geom_text(aes(label = `Residual (Guess)`), hjust = -0.5, size = 3)
+      geom_text_repel(aes(label = `Residual (Guess)`), size = 3)
     } +
     {if (resids_OLS)
       # connects dots to line of OLS
@@ -104,24 +107,27 @@ plot_guess <- function(
       ) 
     } +
     {if (resids_OLS)
-      geom_text(aes(label = `Residual (OLS)`), hjust = 1.25, size = 3) 
+      geom_text_repel(aes(label = `Residual (OLS)`), size = 3) 
     } +
     {if (labels)
-      geom_text(aes(label = 1:10), vjust = -1.25) # label data points
+      geom_text_repel(aes(label = 1:20), size = 3) # label data points
     } +
     theme_classic() +
-    theme(legend.position = "right") +
+    theme(legend.position = "left") +
     scale_color_manual(
       values = wes_palette(2, name = "Moonrise2", type = "discrete"), name = ""
-      ) +
-    scale_y_continuous(limits = c(-4, 14), breaks = seq(-4, 14, 2))
-
+    ) +
+    scale_y_continuous(limits = c(0, 10), breaks = seq(0, 10, 2)) +
+    scale_x_continuous(limits = c(0, 10), breaks = seq(0, 10, 2))
+  
+  plot <- ggMarginal(plot, type = "density")
+  
   return( # ----
-    list(
-      data_tab = data_tab,
-      plot = plot,
-      reg_table_long = reg_table_long
-    )
+          list(
+            data_tab = data_tab,
+            plot = plot,
+            reg_table_long = reg_table_long
+          )
   )
 }
 
@@ -159,8 +165,8 @@ ui <- fluidPage(
       )
     ),
     mainPanel(
-      dataTableOutput("data_tab"),
-      plotOutput("plot")
+      plotOutput("plot", width = 850, height = 500),
+      dataTableOutput("data_tab")
     )
   )
 )
@@ -182,35 +188,33 @@ server <- function(input, output, session) {
       resids_guess = resids_guess(),
       resids_OLS = resids_OLS(),
       labels = labels()
-      )})
-
+    )})
+  
   output$plot <- renderPlot({
     guess_plot()$plot
   }, res = 96)
   
   output$reg_table_long <- renderTable({
     guess_plot()$reg_table_long
-    }, 
+  }, 
   sanitize.text.function=function(x){x}
   )
   
   output$data_tab <- DT::renderDataTable(
     guess_plot()$data_tab, 
-    options = list(pageLength = 3, lengthMenu = 1:10, dom = "plt")
+    options = list(pageLength = 5, lengthMenu = 1:10, dom = "plt")
   )
   
 }
 
 shinyApp(ui = ui, server = server)
 
+# increase sample size DONE
+# move legend to left DONE
+# add marginals DONE  
+# adjust figure dimensions DONE
+# switch to ggrepel
+# if needed, tweak position jitter
+# increase menu size DONE
+# move table to bottom DONE
 
-
-# - DONE create plot function 
-# - DONE create function to calculate sums of squares etc. 
-# - DONE create app that plots scatter plot with slope and y-intercept 
-# - DONE print regression table
-# - DONE print data values table
-# - add options:
-#   - DONE want residual lines to guess line?
-#   - DONE want residual lines to best line?
-#   - DONE want to label data points?
